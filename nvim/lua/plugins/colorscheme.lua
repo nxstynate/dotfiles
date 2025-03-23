9d9c44657ad93356fc4a8a2b203c11c42e5490e4
Nate <nathanael.paul.lewis@gmail.com>
2025-03-22 21:16:14 -0500

    Updated dotfiles on 2025-03-22 21:16:14

--- nvim/lua/plugins/colorscheme.lua
+++ nvim/lua/plugins/colorscheme.lua
@@ -1,41 +1,41 @@
--- return {
---   {
---     "ellisonleao/gruvbox.nvim",
---     opts = {
---       transparent_mode = true, -- Ensure this is a boolean, not a string
---     },
---     config = function(_, opts)
---       require("gruvbox").setup(opts)
---       vim.cmd("colorscheme gruvbox")
---     end,
---   },
---   {
---     "LazyVim/LazyVim",
---     opts = {
---       colorscheme = "gruvbox",
---     },
---   },
--- }
---
 return {
   {
-    "craftzdog/solarized-osaka.nvim",
-    lazy = false,
-    priority = 1000,
+    "ellisonleao/gruvbox.nvim",
     opts = {
-      transparent = true, -- Enable transparency
-      terminal_colors = true,
-      styles = {
-        sidebars = "transparent",
-        floats = "transparent",
-      },
+      transparent_mode = true, -- Ensure this is a boolean, not a string
     },
     config = function(_, opts)
-      require("solarized-osaka").setup(opts)
-      vim.cmd("colorscheme solarized-osaka")
+      require("gruvbox").setup(opts)
+      vim.cmd("colorscheme gruvbox")
     end,
   },
+  {
+    "LazyVim/LazyVim",
+    opts = {
+      colorscheme = "gruvbox",
+    },
+  },
 }
+--
+-- return {
+--   {
+--     "craftzdog/solarized-osaka.nvim",
+--     lazy = false,
+--     priority = 1000,
+--     opts = {
+--       transparent = true, -- Enable transparency
+--       terminal_colors = true,
+--       styles = {
+--         sidebars = "transparent",
+--         floats = "transparent",
+--       },
+--     },
+--     config = function(_, opts)
+--       require("solarized-osaka").setup(opts)
+--       vim.cmd("colorscheme solarized-osaka")
+--     end,
+--   },
+-- }
 
 -- return {
 --   {
