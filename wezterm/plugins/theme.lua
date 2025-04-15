61ecdc86794ac5bc7b7cd5f8f1fad2b759ad1f22
Nate <nathanael.paul.lewis@gmail.com>
2025-04-15 16:05:39 -0500

    Updated dotfiles on 2025-04-15 16:05:39

--- wezterm/plugins/theme.lua
+++ wezterm/plugins/theme.lua
@@ -2,9 +2,10 @@ local M = {}
 
 -- M.current = "Catppuccin Latte"
 -- M.current = "Google (dark) (terminal.sexy)"
--- M.current = "GruvboxDarkHard"
-M.current = "Gruvbox dark, hard (base16)"
+M.current = "GruvboxDarkHard"
+-- M.current = "One Dark (Gogh)"
 -- M.current = "rose-pine"
 -- M.current = "Solarized Dark - Patched"
 -- M.current = "Solarized (dark) (terminal.sexy)"
+-- M.current = "Tokyo Night"
 return M
